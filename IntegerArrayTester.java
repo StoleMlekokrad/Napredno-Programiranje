cf071e51972bfa84311396726a58b9f0b9d39931
croda <cro.davor@outlook.com>
2017-10-19 22:20:10 +0200

    Resenie lab01/zad3

---
+++ IntegerArrayTester.java
@@ -0,0 +1,214 @@
+//import com.sun.org.apache.xalan.internal.xsltc.util.IntegerArray;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+
+
+final class IntegerArray {
+    private final int arr[];
+
+    public int[] getArr() {
+        return arr;
+    }
+
+    public IntegerArray(int[] arr) {
+        this.arr = Arrays.copyOf(arr, arr.length);
+    }
+
+    public int length() {
+        return arr.length;
+    }
+
+    public int getElementAt(int i) {
+        return arr[i];
+    }
+
+    public IntegerArray getSorted() {
+        int[] arr1 = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(arr1);
+        IntegerArray sorted = new IntegerArray(arr1);
+        return sorted;
+    }
+    public int sum() {
+        int s = IntStream.of(arr).sum();
+        return s;
+    }
+
+    public double average() {
+        double result = (double)sum() / length();
+        return result;
+    }
+
+    public IntegerArray concat(IntegerArray ia) {
+        IntegerArray a = ia.getSorted();
+        IntegerArray b = this.getSorted();
+        int len = ia.length() + this.length();
+        int[] ar = new int[ia.length()];
+        int[] ar1 = new int[this.length()];
+
+        for (int i = 0; i < ia.length(); i++) {
+            ar[i] = ia.getElementAt(i);
+        }
+        for (int i = 0; i < this.length(); i++) {
+            ar1[i] = this.getElementAt(i);
+        }
+
+        int[] ar2 = IntStream.concat(Arrays.stream(ar), Arrays.stream(ar1)).toArray();
+		Arrays.sort(ar2);
+        IntegerArray joined = new IntegerArray(ar2);
+        return joined;
+    }
+
+    public boolean equals(Object o) {
+if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        IntegerArray ia = (IntegerArray) o;
+        if (this.length() != ia.length()) return false;
+        for (int i = 0; i < this.length(); i++) {
+        	if (this.getElementAt(i) != ia.getElementAt(i)) return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(arr);
+    }
+}
+
+class ArrayReader {
+    public static IntegerArray readIntegerArray(InputStream input) {
+        Scanner in = new Scanner(input);
+        int n = in.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+        }
+
+        IntegerArray ia = new IntegerArray(a);
+        return ia;
+
+    }
+}
+
+public class IntegerArrayTester {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        IntegerArray ia = null;
+        switch (s) {
+        case "testSimpleMethods":
+            ia = new IntegerArray(generateRandomArray(scanner.nextInt()));
+            testSimpleMethods(ia);
+            break;
+        case "testConcat":
+            testConcat(scanner);
+            break;
+        case "testEquals":
+            testEquals(scanner);
+            break;
+        case "testSorting":
+            testSorting(scanner);
+            break;
+        case "testReading":
+            testReading(new ByteArrayInputStream(scanner.nextLine().getBytes()));
+            break;
+        case "testImmutability":
+            int a[] = generateRandomArray(scanner.nextInt());
+            ia = new IntegerArray(a);
+            testSimpleMethods(ia);
+            testSimpleMethods(ia);
+            IntegerArray sorted_ia = ia.getSorted();
+            testSimpleMethods(ia);
+            testSimpleMethods(sorted_ia);
+            sorted_ia.getSorted();
+            testSimpleMethods(sorted_ia);
+            testSimpleMethods(ia);
+            a[0] += 2;
+            testSimpleMethods(ia);
+            ia = ArrayReader.readIntegerArray(new ByteArrayInputStream(integerArrayToString(ia).getBytes()));
+            testSimpleMethods(ia);
+            break;
+        }
+        scanner.close();
+    }
+
+    static void testReading(InputStream in) {
+        IntegerArray read = ArrayReader.readIntegerArray(in);
+        System.out.println(read);
+    }
+
+    static void testSorting(Scanner scanner) {
+        int[] a = readArray(scanner);
+        IntegerArray ia = new IntegerArray(a);
+        System.out.println(ia.getSorted());
+    }
+
+    static void testEquals(Scanner scanner) {
+        int[] a = readArray(scanner);
+        int[] b = readArray(scanner);
+        int[] c = readArray(scanner);
+        IntegerArray ia = new IntegerArray(a);
+        IntegerArray ib = new IntegerArray(b);
+        IntegerArray ic = new IntegerArray(c);
+        System.out.println(ia.equals(ib));
+        System.out.println(ia.equals(ic));
+        System.out.println(ib.equals(ic));
+    }
+
+    static void testConcat(Scanner scanner) {
+        int[] a = readArray(scanner);
+        int[] b = readArray(scanner);
+        IntegerArray array1 = new IntegerArray(a);
+        IntegerArray array2 = new IntegerArray(b);
+        IntegerArray concatenated = array1.concat(array2);
+        System.out.println(concatenated);
+    }
+
+    static void testSimpleMethods(IntegerArray ia) {
+        System.out.print(integerArrayToString(ia));
+        System.out.println(ia);
+        System.out.println(ia.sum());
+        System.out.printf("%.2f\n", ia.average());
+    }
+
+
+    static String integerArrayToString(IntegerArray ia) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(ia.length()).append('\n');
+        for (int i = 0; i < ia.length(); ++i)
+            sb.append(ia.getElementAt(i)).append(' ');
+        sb.append('\n');
+        return sb.toString();
+    }
+
+    static int[] readArray(Scanner scanner) {
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; ++i) {
+            a[i] = scanner.nextInt();
+        }
+        return a;
+    }
+
+
+    static int[] generateRandomArray(int k) {
+        Random rnd = new Random(k);
+        int n = rnd.nextInt(8) + 2;
+        int a[] = new int[n];
+        for (int i = 0; i < n; ++i) {
+            a[i] = rnd.nextInt(20) - 5;
+        }
+        return a;
+    }
+
+}
